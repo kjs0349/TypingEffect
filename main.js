673ad0ba032f67b62e03199ff98102417c956a3f
Jisu <kjl0349@naver.com>
2021-01-05 02:37:02 +0900

    javascript 파일 주석 처리

--- main.js
+++ main.js
@@ -5,20 +5,21 @@ const messageArry = ['Hello ~', 'My name is', 'Kim Ji Su!!'];
 // dynamic 커서 만들기
 setInterval(function(){
     dynamic.classList.toggle('dynamic')
-}, 200);
+}, 250);
 
 
-let num = 0;
-let message = messageArry[num];
-let printmessage = message.split("");
+let num = 0;    //배열의 인덱스를 늘려 줄 변수 선언
+let message = messageArry[num];     //배열 중 하나의 값을 받아오는 변수 선언
+let printmessage = message.split("");       //받아온 값을 한 문자씩 쪼개서 배열로 담는 변수 선언
 
+//한 문자씩 뿌려주는 함수
 function typingtext(printmessage) {
-    if (printmessage.length > 0) {
+    if (printmessage.length > 0) {      //배열의 첫 번째 문장에 타이핑 효과 주기
         mainText.textContent += printmessage.shift();
         setTimeout(() => {
             typingtext(printmessage);
-        }, 200);
-    } else {
+        }, 250);
+    } else {        //배열의 다음 인덱스로 넘어가기
         num++;
         message = messageArry[num];
         printmessage = message.split("");
