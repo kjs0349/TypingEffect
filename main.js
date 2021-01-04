8aad0ede0a858a5526e9b6a38e8ab8dc9a0d6c55
Jisu <kjl0349@naver.com>
2021-01-05 01:32:28 +0900

    [javascript] 첫 번째 배열 문자열을 한글자씩 타이핑 되도록 설정

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const dynamic = document.querySelector('.dynamic');
 const mainText = document.querySelector('.maintext');
+const messageArry = ['Hello ~', 'My name is', 'KimJiSu!!'];
 
 // dynamic 커서 만들기
 setInterval(function(){
-    mainText.classList.toggle('dynamic')
-}, 300);
+    dynamic.classList.toggle('dynamic')
+}, 400);
+
+
+let num = 0;
+let message = messageArry[num];
+let printmessage = message.split("");
+
+function typingtext(printmessage) {
+    if (printmessage.length > 0) {
+        mainText.textContent += printmessage.shift();
+        setTimeout(() => {
+            typingtext(printmessage);
+        }, 400);
+    } 
+}
+
+typingtext(printmessage);
