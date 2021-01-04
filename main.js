91a51e33aec8d5e74929e5f78da04ccf1031ca2a
Jisu <kjl0349@naver.com>
2021-01-04 22:38:45 +0900

    [css] dynamic 커서 margin, padding 값으로 주면 부자연스러워서 position으로 지정!

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 const dynamic = document.querySelector('.dynamic');
 const mainText = document.querySelector('.maintext');
+
+// dynamic 커서 만들기
+setInterval(function(){
+    mainText.classList.toggle('dynamic')
+}, 300);
